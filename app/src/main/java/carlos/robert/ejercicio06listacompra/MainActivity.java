04aea637c567b517e6f7183f97a958ec386ae2ea
ROB <carlosivan.robertmohedano@cieep.com>
2023-11-03 12:00:27 +0100

    Initial commit

---
+++ app/src/main/java/carlos/robert/ejercicio06listacompra/MainActivity.java
@@ -0,0 +1,118 @@
+package carlos.robert.ejercicio06listacompra;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import carlos.robert.ejercicio06listacompra.adapters.ProductAdapter;
+import carlos.robert.ejercicio06listacompra.databinding.ActivityMainBinding;
+import carlos.robert.ejercicio06listacompra.modelos.Product;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private ArrayList<Product> productList;
+    private ProductAdapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        productList = new ArrayList<>();
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createProduct().show();
+            }
+        });
+    }
+
+    private AlertDialog createProduct() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+        builder.setTitle(R.string.titleCreate);
+        builder.setCancelable(false);
+
+        View productViewModel = LayoutInflater.from(this).inflate(R.layout.product_view_model, null);
+        EditText txtName = productViewModel.findViewById(R.id.txtNameProductViewModel);
+        EditText txtQuantity = productViewModel.findViewById(R.id.txtQuantityProductViewModel);
+        EditText txtPrice = productViewModel.findViewById(R.id.txtPriceProductViewModel);
+        TextView lbTotal = productViewModel.findViewById(R.id.lbTotalProductViewModel);
+        builder.setView(productViewModel);
+
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                try {
+
+
+                    int quantity = Integer.parseInt(txtQuantity.getText().toString());
+                    float price = Float.parseFloat(txtPrice.getText().toString());
+                    float total = quantity * price;
+
+                    lbTotal.setText(String.valueOf(total)+" $");
+                }catch (Exception e){
+
+                }
+
+            }
+        };
+
+        txtQuantity.addTextChangedListener(textWatcher);
+        txtPrice.addTextChangedListener(textWatcher);
+
+        builder.setNegativeButton(R.string.cancel, null);
+        builder.setPositiveButton(R.string.accept, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (txtName.getText().toString().isEmpty() ||
+                        txtQuantity.getText().toString().isEmpty() ||
+                        txtPrice.getText().toString().isEmpty()) {
+                    Toast.makeText(MainActivity.this, R.string.missing, Toast.LENGTH_SHORT).show();
+                } else {
+                    Product product = new Product(
+                            txtName.getText().toString(),
+                            Integer.parseInt(txtQuantity.getText().toString()),
+                            Float.parseFloat(txtPrice.getText().toString())
+                    );
+                    productList.add(0, product);
+
+                    /*
+                     *TODO: redibujar el recyclerview
+                     */
+                    Toast.makeText(MainActivity.this, product.toString(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        return builder.create();
+    }
+}
